76731ff622e4add3e5dddfea697b41f1c62bdaa4
Gleydson Rocha <gleydson@gwrocha.com>
2020-05-08 00:28:41 -0300

    Configuração de conversão de objetos para json.

---
+++ src/main/java/com/gwrocha/vancol/config/ObjectMapperConfiguration.java
@@ -0,0 +1,29 @@
+package com.gwrocha.vancol.config;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+@Configuration
+public class ObjectMapperConfiguration {
+	
+	@Bean
+	public Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder() {
+	    Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+	    builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+	    builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+	    builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern("HHmm")));
+	    builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+	    builder.deserializers(new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HHmm")));
+	    return builder;
+	}
+	
+}
